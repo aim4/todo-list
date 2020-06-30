6efc62bb43bb232992742754a15be202bf4b81df
Aim4 <aimforking@gmail.com>
2020-06-30 01:31:54 -0400

    Add button functions for adding task and clearing input

--- script.js
+++ script.js
@@ -19,11 +19,38 @@ class TaskManager {
 }
 
 class Task {
-    constructor() {
-
+    constructor(t) {
+        this.text = t;
+        this.completed = false;
     }
 
     setText(t) {
         this.text = t;
     }
 }
+
+function clearInput(e) {
+    //e.stopPropagation();
+    let input = document.getElementById("task-input");
+    input.value = "";
+}
+
+function addTask(e) {
+    //e.stopPropagation();
+    let input = document.getElementById("task-input");
+    if (input.value === "") {
+        // TODO add a warning
+        console.log("No input");
+        return;
+    }
+
+    let t = new Task(input.value);
+    console.log("created new task: ", t);
+}
+
+// Bind button functions
+let addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addTask);
+
+let clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", clearInput);
