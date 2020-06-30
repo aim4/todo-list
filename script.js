a5784cf7f9961a4a2cc56c836e01afdaca934e76
Aim4 <aimforking@gmail.com>
2020-06-30 01:22:15 -0400

    Add skeleton of Task and TaskManager classes

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// JavaScript source code
+class TaskManager {
+    constructor() {
+        this.tasks = []
+    }
+
+    addTask(t) {
+        this.tasks.push(t);
+    }
+
+    // Remove task at index if exists
+    removeTask(i) {
+
+    }
+
+    // Get task at index
+    getTask(i) {
+
+    }
+}
+
+class Task {
+    constructor() {
+
+    }
+
+    setText(t) {
+        this.text = t;
+    }
+}
